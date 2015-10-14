0c614d7bb21a1ba4b2cbe0c278cc94a22d13dd0f
septiara11 <septiarasi@gmail.com>
2015-10-14 20:37:49 +0700

    TUGAS2_PBOJAVA

---
+++ src/septiara14311685/OperatorAritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package septiara14311685;
+
+/**
+ *
+ * @author User
+ */
+public class OperatorAritmatika {
+  public int nilaihasil;
+ 
+ int penjumlahan (int x,int y){
+   nilaihasil=x+y;
+   return nilaihasil;
+   }
+ 
+ int pengurangan (int x,int y){
+   nilaihasil=x-y;
+   return nilaihasil;
+   }
+ int perkalian (int x,int y){
+   nilaihasil=x*y;
+   return nilaihasil;
+   }
+ double pembagian (double x,double y){
+   nilaihasil= (int) (x/y);
+   return nilaihasil;
+ }
+
+   
+}
